bd21c9fb65e69b4717cbdad28824664a6c8e0a05
Yasmin Manovel <yasminmanovel@outlook.com>
2020-02-28 15:16:55 +1100

    Update PongWk3.py

--- PongWk3.py
+++ PongWk3.py
@@ -1,14 +1,25 @@
-
+# create dotted line
+line = Turtle()
+line.penup()
+line.goto(0, 160)
+line.right(90)
+line.color("white")
+for num in range(16):
+  line.penup()
+  line.forward(10)
+  line.pendown()
+  line.forward(10)
 
 # Pen
 pen = Turtle()
-pen.speed(0)
-pen.shape("square")
-
 pen.penup()
 pen.hideturtle()
-pen.goto(0, 260/2)
+pen.goto(0, 130)
 pen.color("red")
 pen.write("  Player 2: 0", align="left", font=("Courier", 12, "normal"))
 pen.color("blue")
 pen.write("Player 1: 0  ", align="right", font=("Courier", 12, "normal"))
+
+
+
+background.update()
