b00f39e2cc5633bf314aca34ac9d0f367a8c0bef
Yasmin Manovel <yasminmanovel@outlook.com>
2020-02-28 14:28:50 +1100

    Create PongWk2.py

---
+++ PongWk2.py
@@ -0,0 +1,28 @@
+# Player 1
+p1 = Turtle()
+p1.speed(0)
+p1.shape("square")
+p1.color("blue")
+p1.penup()
+p1.goto(-190, 0)
+background.update()
+
+# Player 2
+p2 = Turtle()
+p2.speed(0)
+p2.shape("square")
+p2.color("red")
+p2.penup()
+p2.goto(190, 0)
+background.update()
+
+# Create ball
+ball = Turtle()
+ball.speed(0)
+ball.shape("circle")
+ball.color("white")
+ball.penup()
+ball.goto(0, 0)
+ball.dx = 2
+ball.dy = 2
+background.update()
